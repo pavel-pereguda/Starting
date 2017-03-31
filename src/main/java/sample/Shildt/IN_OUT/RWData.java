59450e35d28934b0ab7a48c9c54e469e456d641d
Pavel <q3kasper@gmail.com>
2017-03-31 17:19:00 +0400

    main

---
+++ src/main/java/sample/Shildt/IN_OUT/RWData.java
@@ -0,0 +1,43 @@
+package sample.Shildt.IN_OUT;
+
+import java.io.*;
+
+/**
+ * Created by pavel on 28.03.2017.
+ */
+public class RWData {
+    public static void main(String[] args) {
+        int i = 10;
+        double d = 1023.56;
+        boolean b = true;
+
+        try (DataOutputStream dataOut = new DataOutputStream(new FileOutputStream("qw"))) {
+            System.out.println("Writed: " + i);
+            dataOut.writeInt(i);
+            System.out.println("Writed: " + d);
+            dataOut.writeDouble(d);
+            System.out.println("Writed: " + b);
+            dataOut.writeBoolean(b);
+            System.out.println("Writed: " + 12.2 * 7.4);
+            dataOut.writeDouble(12.2 * 7.4);
+        } catch (IOException e) {
+            System.out.println("Write err");
+            return;
+        }
+        System.out.println();
+        try (DataInputStream dataIn = new DataInputStream(new FileInputStream("qw")))
+        {
+            i = dataIn.readInt();
+            System.out.println("Readed:" + i);
+            d = dataIn.readDouble();
+            System.out.println("Readed:" + d);
+            b = dataIn.readBoolean();
+            System.out.println("Readed:" + b);
+            d = dataIn.readDouble();
+            System.out.println("Readed:" + d);
+        } catch (IOException e){ ;
+            System.out.println("Reading err");
+        }
+    }
+}
+//hello to all humans
---
+++ src/main/java/sample/Shildt/IN_OUT/ShowFile.java
@@ -0,0 +1,39 @@
+package sample.Shildt.IN_OUT;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class ShowFile {
+    public static void main(String[] args) {
+        int i;
+        FileInputStream fin = null;
+        if (args.length != 1) {
+            System.out.println("Using Show file TEXT");
+        }
+        try {
+            fin = new FileInputStream("C://Read.txt");
+            do{
+                i=fin.read();
+                if(i!=-1)
+                    System.out.print((char)i);
+            }while (i!=-1);
+        } catch (FileNotFoundException exc) {
+            System.out.println("File not found");
+            return;
+
+        }catch (IOException exc){
+            System.out.println("Input-Output err");
+
+        }finally {
+            try
+            {
+                if(fin!=null)
+                    fin.close();
+            }catch (IOException exc){
+                System.out.println("Error while closing file");
+            }
+        }
+    }
+}
